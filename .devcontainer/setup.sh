17fc554ed64672c4564a01192da500fa19884667
Brady <bradyg@microsoft.com>
2020-03-13 06:03:55 +0000

    edited setup file

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,9 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
 cd ~/
 mkdir .kube
-cp config ~/.kube
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+cp config ~/.kube/config
